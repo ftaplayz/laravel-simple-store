9e2c227c8bb5099eae422f5afcbb45ca4230bca1
ftaplayz <abiliomatute1@gmail.com>
2025-01-31 11:48:18 +0000

    route changes, modified welcome view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Controllers\UserController::class, 'index']);
